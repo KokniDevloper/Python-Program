2191a75f64e79c337cb50b77109219b3bf9c41c1
KokniDevloper <koknisadique402@gmail.com>
2025-05-15 20:03:41 +0530

    Random dice roller in python #Project 2

---
+++ dice.py
@@ -0,0 +1,11 @@
+import random
+
+totaldice = int(input("How many dice do you want to roll? "))
+print(f"Rolling {totaldice} dice...")
+print("Each die have 6 sides.")
+totalvalue = 0
+for i in range(totaldice):
+    dice = random.randint(1, 6)
+    print(f"Dice {i + 1}: {dice}")
+    totalvalue += dice
+print(f"Total value of all dice: {totalvalue}")
