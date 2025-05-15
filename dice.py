5a0b4384b542d710c338c42a34ece01b9ed1d362
KokniDevloper <koknisadique402@gmail.com>
2025-05-15 20:11:47 +0530

    Add random Multiple Dice Roll program using Python

--- dice.py
+++ dice.py
@@ -1,11 +1,17 @@
 import random
 
-totaldice = int(input("How many dice do you want to roll? "))
-print(f"Rolling {totaldice} dice...")
-print("Each die have 6 sides.")
-totalvalue = 0
-for i in range(totaldice):
-    dice = random.randint(1, 6)
-    print(f"Dice {i + 1}: {dice}")
-    totalvalue += dice
-print(f"Total value of all dice: {totalvalue}")
+def main():
+    print("Welcome to the Dice Roller!")
+    print("This program will roll a specified number of dice and display the results.")
+    totaldice = int(input("How many dice do you want to roll? "))
+    print(f"Rolling {totaldice} dice...")
+    print("Each die have 6 sides.")
+    totalvalue = 0
+    for i in range(totaldice):
+        dice = random.randint(1, 6)
+        print(f"Dice {i + 1}: {dice}")
+        totalvalue += dice
+    print(f"Total value of all dice: {totalvalue}")
+
+    if __name__ == "__main__":
+        main()
